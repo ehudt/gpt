2e29e5bbb8aa9c11de15d87298e0bed96adca3d6
Ehud Tamir <1566271+ehudt@users.noreply.github.com>
2023-04-19 17:56:53 +0300

    ts version of orchestration
    reviewed the diff quickly. didn't thoroughly review

---
+++ orchestration.ts
@@ -0,0 +1,247 @@
+import AWS from 'aws-sdk';
+
+AWS.config.update({ region: 'your-region' });
+
+const dynamoDB = new AWS.DynamoDB();
+
+interface StepData {
+  [key: string]: any;
+}
+
+interface StepResult {
+  [key: string]: any;
+}
+
+async function initializeDatabase() {
+    const params: AWS.DynamoDB.CreateTableInput = {
+        TableName: 'PaymentStates',
+        KeySchema: [
+            { AttributeName: 'paymentId', KeyType: 'HASH' },
+            { AttributeName: 'stepName', KeyType: 'RANGE' },
+        ],
+        AttributeDefinitions: [
+            { AttributeName: 'paymentId', AttributeType: 'S' },
+            { AttributeName: 'stepName', AttributeType: 'S' },
+        ],
+        ProvisionedThroughput: {
+            ReadCapacityUnits: 5,
+            WriteCapacityUnits: 5,
+        },
+    };
+
+    try {
+        await dynamoDB.createTable(params).promise();
+    } catch (err) {
+        if (err.code !== 'ResourceInUseException') {
+            console.error('Error initializing DynamoDB table:', err);
+            throw err;
+        }
+    }
+}
+
+initializeDatabase();
+
+abstract class PaymentStep {
+    abstract async execute(data: StepData, idempotencyKey: string): Promise<void>;
+}
+
+class BankAccountVerification extends PaymentStep {
+    async execute(data: StepData, idempotencyKey: string): Promise<void> {
+        // Implement the special verification method for bank account using the idempotency key
+    }
+}
+
+class CreditCardVerification extends PaymentStep {
+    async execute(data: StepData, idempotencyKey: string): Promise<void> {
+        // Implement the special verification method for bank account using the idempotency key
+    }
+}
+
+class BankAccountProcessing extends PaymentStep {
+    async execute(data: StepData, idempotencyKey: string): Promise<void> {
+        // Implement the special verification method for bank account using the idempotency key
+    }
+}
+
+class CreditCardProcessing extends PaymentStep {
+    async execute(data: StepData, idempotencyKey: string): Promise<void> {
+        // Implement the special verification method for bank account using the idempotency key
+    }
+}
+
+abstract class PaymentMethod {
+    constructor(protected steps: PaymentStep[]) {}
+
+    async executeSteps(data: StepData): Promise<StepResult> {
+        const results: StepResult = {};
+        for (const step of this.steps) {
+            const stepResult = await step.execute(data);
+            results[step.constructor.name] = stepResult;
+        }
+        return results;
+    }
+    abstract getRequiredFields(): string[];
+}
+
+class BankAccountPayment extends PaymentMethod {
+    constructor() {
+        super([new BankAccountVerification(), new BankAccountProcessing()]);
+    }
+  
+    getRequiredFields(): string[] {
+        return ['account_number', 'routing_number', 'full_name'];
+    }
+}
+
+class CreditCardPayment extends PaymentMethod {
+    constructor() {
+        super([new CreditCardVerification(), new CreditCardProcessing()]);
+    }
+  
+    getRequiredFields(): string[] {
+        return ['card_number', 'billing_address', 'cardholder_name'];
+    }
+}
+
+const { DynamoDB } = AWS;
+const docClient = new DynamoDB.DocumentClient();
+
+interface PaymentMethods {
+  [key: string]: PaymentMethod;
+}
+
+class PaymentWorkflow {
+    constructor(
+        private paymentMethods: PaymentMethods,
+        private maxRetries: number = 3,
+        private retryDelay: number = 5000
+    ) {}
+
+    getRequiredFields(paymentMethodId: string): string[] {
+        const paymentMethod = this.paymentMethods[paymentMethodId];
+        if (!paymentMethod) {
+            throw new Error(`Unknown payment method: ${paymentMethodId}`);
+        }
+        return paymentMethod.getRequiredFields();
+    }
+
+    async executeStepWithRetry(step: PaymentStep, data: StepData, idempotencyKey: string): Promise<{ result: any; success: boolean }> {
+        for (let attempt = 1; attempt <= this.maxRetries; attempt++) {
+            try {
+                const result = await step.execute(data, idempotencyKey);
+                return { result, success: true };
+            } catch (err) {
+                console.error(`Step ${step.constructor.name} failed on attempt ${attempt}:`, err);
+                await new Promise((resolve) => setTimeout(resolve, this.retryDelay));
+            }
+        }
+        return { result: null, success: false };
+    }
+
+    async persistStepResult(paymentId: string, stepName: string, data: StepData, result: any, success: 'success' | 'failed'): Promise<void> {
+        const params: AWS.DynamoDB.DocumentClient.PutItemInput = {
+            TableName: 'PaymentStates',
+            Item: {
+                paymentId,
+                stepName,
+                data,
+                result,
+                success,
+            },
+        };
+
+        try {
+            await docClient.put(params).promise();
+        } catch (err) {
+            console.error('Error persisting step result:', err);
+            throw err;
+        }
+    }
+
+    async executePaymentSteps(paymentMethodId: string, data: StepData): Promise<StepResult> {
+        const paymentMethod = this.paymentMethods[paymentMethodId];
+        if (!paymentMethod) {
+            throw new Error(`Unknown payment method: ${paymentMethodId}`);
+        }
+
+        const results: StepResult = {};
+        for (const step of paymentMethod.steps) {
+            const idempotencyKey = `${paymentId}_${step.constructor.name}`;
+            const { result, success } = await this.executeStepWithRetry(step, data, idempotencyKey);
+            await this.persistStepResult(paymentId, step.constructor.name, data, result, success ? 'success' : 'failed');
+            results[step.constructor.name] = result;
+            if (!success) {
+                break;
+            }
+        }
+
+        return results;
+    }
+
+    async resumePaymentSteps(paymentId: string): Promise<void> {
+        const params: AWS.DynamoDB.DocumentClient.QueryInput = {
+            TableName: 'PaymentStates',
+            KeyConditionExpression: 'paymentId = :paymentId',
+            ExpressionAttributeValues: {
+                ':paymentId': paymentId,
+            },
+        };
+
+        const data = await docClient.query(params).promise();
+        const items = data.Items;
+
+        if (items.length === 0) {
+            throw new Error(`Unknown payment method ID: ${paymentId}`);
+        }
+
+        const { paymentMethodId, stepName, data: stepData, result, success } = items[items.length - 1];
+
+        if (success) {
+            console.log(`Payment process ${paymentId} already completed`);
+            return;
+        }
+
+        const paymentMethod = this.paymentMethods[paymentMethodId];
+        if (!paymentMethod) {
+            throw new Error(`Unknown payment method: ${paymentMethodId}`);
+        }
+
+        let startNextStep = false;
+        for (const step of paymentMethod.steps) {
+            if (startNextStep || step.constructor.name === stepName) {
+                startNextStep = true;
+                const { result, success } = await this.executeStepWithRetry(step, stepData);
+                await this.persistStepResult(paymentId, step.constructor.name, stepData, result, success);
+                if (!success) {
+                    break;
+                }
+            }
+        }
+    }
+}
+
+// Initialize the payment methods and create a new PaymentWorkflow instance
+const paymentMethods: PaymentMethods = {
+    bank_account: new BankAccountPayment(),
+    credit_card: new CreditCardPayment(),
+};
+
+const workflow = new PaymentWorkflow(paymentMethods);
+
+// Get required fields for a specific payment method from the UI
+const requiredFields = workflow.getRequiredFields('credit_card');
+
+// Execute the sub-steps for a specific payment method and store the payment_id
+const data: StepData = {
+    // ... data obtained from the UI ...
+};
+
+const paymentId = 'some_unique_identifier';
+const results = await workflow.executePaymentSteps('credit_card', data);
+
+// Save the paymentId somewhere safe (e.g., in a log file or a separate database table)
+
+// After a machine reboot or other interruption, retrieve the paymentId and resume the payment steps
+await workflow.resumePaymentSteps(paymentId);
+
+
